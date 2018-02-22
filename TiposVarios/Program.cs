e725254a2d1d475a6c732466ad5ebb59b8baf621
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2018-02-22 10:21:47 +0000

    Tentativa com varios tipos de strings

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Isto e uma string normal";
+            string t = @"isto éuma string verbatim posso usar / sem chatiçes ";
+            string v = "\" \u00A9\"";
+           Console.WriteLine(s);
+            Console.WriteLine(t);
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+    }
+}
