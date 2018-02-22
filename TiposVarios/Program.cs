3a12e24b3d639c9ecdc0ce4d5f2d8f1fe5130381
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2018-02-22 10:36:51 +0000

    Strings interpoladas e interpoladas+verbatim adicionadas

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -14,8 +14,11 @@ namespace TiposVarios
             string t = @"isto é uma string verbatim posso usar / sem chatiçes ";
             string v = "\" \u00A9\"";
             string x = "gosto " + "de " + "gelado";
-            x += "no verao";
-            
+            x += " no verao";
+            string j = $"{5} mais {3} e {5 + 3} ";
+            string u = $@"string verbatim e interpolada / {7}";
+            Console.WriteLine(j);
+            Console.WriteLine(u);
             Console.WriteLine(s);
             Console.WriteLine(t);
             Console.WriteLine(v);
